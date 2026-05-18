6f694f6a0570114bab2b2b30619a1f9b5356f05f
dhanyashreemh <dhanyashree@raakacloud.com>
2026-05-18 16:54:37 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black">
+      <h1 className="text-5xl font-bold text-cyan-400">RelayHub 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
